dd3a88cac0ad6c79b2562f30ea5e95a98d70c3e5
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-10 20:37:54 +0100

    Added Submit event

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import "./App.css";
 
 function App() {
+  function searchCity(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={searchCity}>
         <input type="search" placeholder="Enter a city..." />
         <input type="submit" value="Search" />
       </form>
