a132ffb36027c0b21eaa596a5344c0b6ceb71c7e
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-10 20:40:18 +0100

    Added input onChange event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+
   function searchCity(event) {
     event.preventDefault();
+  }
+
+  function inputCity(city) {
+    setCity(city.target.value);
   }
 
   return (
     <div className="App">
       <form onSubmit={searchCity}>
-        <input type="search" placeholder="Enter a city..." />
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          onChange={inputCity}
+        />
         <input type="submit" value="Search" />
       </form>
+      <p>{city}</p>
     </div>
   );
 }
