50b435d4dedf9accda976da0e9789087a8a5180e
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-04-11 12:15:56 +0100

    Added info only when a city is searched

--- src/DisplayWeather.js
+++ src/DisplayWeather.js
@@ -1,20 +1,22 @@
 export default function DisplayWeather(props) {
-  return (
-    <div className="DisplayWeather">
-      <div>
-        <h1>
-          {props.weather.city} ({props.weather.country})
-        </h1>
-        <h3>Thursday 15:00</h3>
-        <h3>{props.weather.description}</h3>
-      </div>
-      <div>
+  if (props.weather) {
+    return (
+      <div className="DisplayWeather">
         <div>
-          <h2>{props.weather.temperature}˚C</h2>
+          <h1>
+            {props.weather.city} ({props.weather.country})
+          </h1>
+          <h3>Thursday 15:00</h3>
+          <h3>{props.weather.description}</h3>
         </div>
-        <h3>{props.weather.humidity}%</h3>
-        <h3>{props.weather.windSpeed}km/h</h3>
+        <div>
+          <div>
+            <h2>{props.weather.temperature}˚C</h2>
+          </div>
+          <h3>{props.weather.humidity}%</h3>
+          <h3>{props.weather.windSpeed}km/h</h3>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
